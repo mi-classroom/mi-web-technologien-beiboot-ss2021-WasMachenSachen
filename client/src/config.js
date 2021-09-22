b889d62785e81fb99381540a6ca134ef0b23899e
Tim Loges <loges.info@googlemail.com>
2021-09-22 17:34:07 +0200

    feat: new config structure

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,31 @@
+/* https://exiftool.org/TagNames/IPTC.html */
+const jsonStringifySize = 9;
 export const config = {
   baseUrl: "http://localhost:3030",
-  imgTags: ["Artist", "ImageDescription", "Country", "Writer"],
+  imgTags: [
+    {
+      tag: "Headline",
+      label: { de: "Titel", en: "Title" },
+      content: { de: "", en: "" },
+      max: 256 - jsonStringifySize,
+    },
+    {
+      tag: "ImageDescription",
+      label: { de: "Dateiart / Beschreibung", en: "Type / Description" },
+      content: { de: "", en: "" },
+      max: 100 - jsonStringifySize,
+    },
+    {
+      tag: "Writer",
+      label: { de: "Autor / Rechte", en: "Author / Copyright" },
+      content: { de: "", en: "" },
+      max: 32 - jsonStringifySize,
+    },
+    {
+      tag: "Source",
+      label: { de: "Quelle", en: "Source" },
+      content: { de: "", en: "" },
+      max: 32 - jsonStringifySize,
+    },
+  ],
 };
